12ff2302a247080e5bfcf40f19158e4304cc7637
pkingsto <pkingsto@amazon.com>
2022-07-19 21:45:39 +0530

    :
    [Problem]
    
    [Solution]
    
    [Testing]
    
    [CX impact]
    
    [Integration Risks]
    
    [Potential Impact to cpu, memory, power, thermal or latency]
    
    [Scope of testing request to QA]
    
    [A link to the design doc this patch is associated with]
    
    [A list of teams affected by this change]
    
    [Links]
    https://issues.labcollab.net/browse/

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Login from './Login';
+import Profile from './Profile';
+import ColorSelector from './ColorSelector';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Profile/>
+      <Login/>     
+      <ColorSelector/> 
+    </div>
+  );
+}
+
+export default App;
