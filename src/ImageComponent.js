a134be52865a5eb2ce5fa4a62b762f1749b64079
PrinceKingston14 <prince14596@gmail.com>
2022-07-21 22:00:40 +0530

    Add Image Component

---
+++ src/ImageComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImageComponent = () => {
+  return (
+    <img
+      src="https://cdn2.iconfinder.com/data/icons/audio-16/96/user_avatar_profile_login_button_account_member-512.png"
+      alt="ReduxImage"
+      width="50px"
+    />
+  );
+};
+
+export default ImageComponent;
